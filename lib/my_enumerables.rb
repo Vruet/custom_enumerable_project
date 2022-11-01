3dd9ee0fb952c14d7fea5adaf6dd5c81d32e24dc
Vruet <vruet3@gmail.com>
2022-11-01 15:56:42 -0600

    Add multiple methods
    Added my_each, my_each_with_index, my_all?, my_any?, my_count

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,44 @@
+# frozen_string_literal: true
+
+# My recreations of enumerables
 module Enumerable
   # Your code goes here
+  def my_all?
+    my_each { |item| return false unless yield item }
+    true
+  end
+
+  def my_any?
+    my_each { |item| return true if yield item }
+    false
+  end
+
+  def my_count
+    if block_given?
+      count = 0
+      my_each do |item|
+        count += 1 if yield item
+      end
+      count
+    else
+      length
+    end
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
+# Contains recreated array methods
 class Array
-  # Define my_each here
+  def my_each
+    for item in self
+      yield item
+    end
+  end
+
+  def my_each_with_index
+    index = 0
+    for item in self
+      yield item, index
+      index += 1
+    end
+  end
 end
